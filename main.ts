5378b142140ff47c061dc17c92b4553dc38f8670
WrenMCarter <wren.carter@student.csd509j.net>
2025-06-09 18:39:31 +0000

    updated splitApart function

--- main.ts
+++ main.ts
@@ -74,15 +74,30 @@ function characterLoop(characters: string, start: number, end: number) {
 }
 
 function splitApart(baseText: string, first: number) {
-    if (first == 1) {
-        return characterLoop(baseText, 0, 1)
-    } else if (first == 2) {
-        return characterLoop(baseText, 2, 4)
+    let oneDigit = false
 
-    } else {
-        return characterLoop(baseText, 6, 10)
+    if (baseText.charAt(2) == ":") {
+        oneDigit = true
     }
+    if (oneDigit) {
+        if (first == 1) {
+            return baseText.charAt(0)
+        } else if (first == 2) {
+            return characterLoop(baseText, 1, 3)
+        } else {
+            return characterLoop(baseText, 5, 9)
+        }
+    } else {
+        if (first == 1) {
+            return characterLoop(baseText, 0, 1)
+        } else if (first == 2) {
+            return characterLoop(baseText, 2, 4)
+        } else {
+            return characterLoop(baseText, 6, 10)
+        }
+    
 
+    }
 }
     
 function setUp(startwithmil: boolean) {
@@ -94,7 +109,7 @@ function setUp(startwithmil: boolean) {
         lettercount = 5
     } else {
         message = "What time is it?"
-        game.splash("Enter time in this format: XX:XX x.m.")
+        game.splash("Enter time in this format: XX:XX x.m. or X:XX x.m.")
         lettercount = 10
     }
     startTime = game.askForString(message, lettercount)
@@ -118,7 +133,7 @@ if (choice == 1) {
 } else {
     setUp(false)
     let ampmboolean = true
-    
+
     if (splitApart(startTime, 3) == "p.m.") {
         ampmboolean = false
     }
